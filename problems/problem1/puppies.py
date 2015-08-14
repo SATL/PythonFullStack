f0c81b4619c7d1c2b6c82120eaac07c7feb14ba0
Eslem <j.eslem03@gmail.com>
2015-08-14 16:47:18 +0200

    relatonships

--- problems/problem1/puppies.py
+++ problems/problem1/puppies.py
@@ -1,9 +1,12 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, Date, Numeric
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Date, Numeric
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
  
 Base = declarative_base()
+
+association_table = Table('association', Base.metadata, Column('puppy_id', Integer, ForeignKey('puppy.id')), Column('adopters_id', Integer, ForeignKey('adopters.id')))
 
 class Shelter(Base):
     __tablename__ = 'shelter'
@@ -25,6 +28,24 @@ class Puppy(Base):
     shelter_id = Column(Integer, ForeignKey('shelter.id'))
     shelter = relationship(Shelter)
     weight = Column(Numeric(10))
+    profile = relationship("PuppyProfile", uselist=False, backref="puppy")
+    adopters = relationship("Adopters", secondary=association_table, backref="puppies")
+
+class PuppyProfile(Base):
+    __tablename__='puppy_profile'
+    id = Column(Integer, primary_key=True)
+    photo = Column(String(80))
+    description = Column(String(250))
+    needs = Column(String(100))
+    puppy_id = Column(Integer, ForeignKey('puppy.id'))
+
+class Adopters(Base):
+    __tablename__='adopters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    address = Column(String(100))
+    phone = Column(String(80))
+
 
 
 engine = create_engine('sqlite:///puppyshelter.db')
